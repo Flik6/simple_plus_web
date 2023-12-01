a49a71ab5be79cb5f6c3b8b5b354f9848b1c748a
hupeng <guchengwuyue@163.com>
2023-12-01 17:26:33 +0800

    积分订单完成

--- src/views/score/order/index.vue
+++ src/views/score/order/index.vue
@@ -1,5 +1,16 @@
 <template>
   <ContentWrap>
+    <el-tabs v-model="activeName"  @tab-click="handleClick">
+      <el-tab-pane label="全部订单" name="first"/>
+    </el-tabs>
+    <el-form-item label="订单状态：" >
+      <el-radio-group v-model="orderStatus" size="large"  fill="#DC143C" @change="queryOrderStatus">
+        <el-radio-button label="-1">全部</el-radio-button>
+        <el-radio-button label="0">待发货</el-radio-button>
+        <el-radio-button label="1">待收货</el-radio-button>
+        <el-radio-button label="2">已完成</el-radio-button>
+      </el-radio-group>
+    </el-form-item>
     <!-- 搜索工作栏 -->
     <el-form
       class="-mb-15px"
@@ -10,8 +21,8 @@
     >
       <el-form-item label="用户名" prop="userId">
         <el-input
-          v-model="queryParams.userId"
-          placeholder="请输入用户名"
+          v-model="queryParams.customerPhone"
+          placeholder="请输入电话"
           clearable
           @keyup.enter="handleQuery"
           class="!w-240px"
@@ -20,21 +31,16 @@
 
       <el-form-item label="订单号" prop="number">
         <el-input
-          v-model="queryParams.number"
+          v-model="queryParams.orderId"
           placeholder="请输入订单号"
           clearable
           @keyup.enter="handleQuery"
           class="!w-240px"
         />
-      </el-form-item>>
-    
-     
+      </el-form-item>
       <el-form-item>
         <el-button @click="handleQuery"><Icon icon="ep:search" class="mr-5px" /> 搜索</el-button>
         <el-button @click="resetQuery"><Icon icon="ep:refresh" class="mr-5px" /> 重置</el-button>
-        <el-button type="primary" @click="openForm('create')" v-hasPermi="['score:order:create']">
-          <Icon icon="ep:plus" class="mr-5px" /> 新增
-        </el-button>
         <el-button
           type="success"
           plain
@@ -52,28 +58,51 @@
   <ContentWrap>
     <el-table v-loading="loading" :data="list">
       <el-table-column label="id" align="center" prop="id" />
-      <el-table-column label="用户id" align="center" prop="userId" />
-      <el-table-column label="商品id" align="center" prop="productId" />
-      <el-table-column label="数量" align="center" prop="number" />
-      <el-table-column label="单个商品积分" align="center" prop="score" />
+      <el-table-column label="订单号" align="center" prop="orderId" width="190" />
+      <el-table-column label="用户id|昵称" align="center" prop="uid" width="150">
+        <template #default="scope">
+          <span>{{ scope.row.uid }}|{{ scope.row.userRespVO.nickname }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="商品信息" align="center" prop="productId" width="350">
+        <template #default="scope">
+          <div class="tabBox">
+              <div class="tabBox_img">
+                  <img :src="scope.row.scoreProductRespVO.image" />
+              </div>
+              <span class="tabBox_tit">{{ scope.row.scoreProductRespVO.title }}</span>
+              <span class="tabBox_pice">{{ '积分'+ scope.row.scoreProductRespVO.score + ' x '+ scope.row.number}}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <!-- <el-table-column label="数量" align="center" prop="number" /> -->
       <el-table-column label="总消耗积分" align="center" prop="totalScore" />
-      <el-table-column label="下单ip" align="center" prop="ip" />
-      <el-table-column label="快递编号" align="center" prop="expressNumber" />
-      <el-table-column label="快递公司" align="center" prop="expressCompany" />
-      <el-table-column label="收货名称" align="center" prop="customerName" />
-      <el-table-column label="收货电话" align="center" prop="customerPhone" />
-      <el-table-column label="收货地址" align="center" prop="customerAddress" />
-      <el-table-column label="订单状态:0=取消订单,1=正常啊" align="center" prop="status" />
-      <el-table-column label="已支付:0=否" align="center" prop="havePaid" />
-      <el-table-column label="已发货:0=否" align="center" prop="haveDelivered" />
-      <el-table-column label="已收货:0=否" align="center" prop="haveReceived" />
+      <el-table-column label="是否支付" align="center" prop="havePaid">
+        <template #default="scope">
+           <span v-if="scope.row.havePaid == 0" style="color:#F56C6C">否</span>
+           <span v-if="scope.row.havePaid == 1" style="color:#409EFF">是</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="是否发货" align="center" prop="haveDelivered">
+        <template #default="scope">
+           <span v-if="scope.row.haveDelivered == 0" style="color:#F56C6C">否</span>
+           <span v-if="scope.row.haveDelivered == 1" style="color:#409EFF">是</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="是否收货" align="center" prop="haveReceived">
+        <template #default="scope">
+           <span v-if="scope.row.haveReceived == 0" style="color:#F56C6C">否</span>
+           <span v-if="scope.row.haveReceived == 1" style="color:#409EFF">是</span>
+        </template>
+      </el-table-column>
       <el-table-column
         label="添加时间"
         align="center"
         prop="createTime"
         :formatter="dateFormatter"
+        width="170"
       />
-      <el-table-column label="操作" align="center">
+      <el-table-column label="操作" align="center" fixed="right" width="150">
         <template #default="scope">
           <el-button
             link
@@ -81,16 +110,18 @@
             @click="openForm('update', scope.row.id)"
             v-hasPermi="['score:order:update']"
           >
-            编辑
+            发货
           </el-button>
-          <el-button
-            link
-            type="danger"
-            @click="handleDelete(scope.row.id)"
-            v-hasPermi="['score:order:delete']"
-          >
-            删除
-          </el-button>
+          <el-dropdown>
+            <el-button type="primary" link><Icon icon="ep:d-arrow-right" /> 更多</el-button>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item @click="openForm('orderDetail', scope.row.id)">订单详情</el-dropdown-item>
+                <el-dropdown-item @click="handleDelete(scope.row.id)">删除订单</el-dropdown-item>
+                <el-dropdown-item v-if = "scope.row.statusStr == '待收货'" @click="handleTake(scope.row.id)">后台收货</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
         </template>
       </el-table-column>
     </el-table>
@@ -105,6 +136,7 @@
 
   <!-- 表单弹窗：添加/修改 -->
   <OrderForm ref="formRef" @success="getList" />
+  <OrderDetail ref="formRef4" />
 </template>
 
 <script setup lang="ts" name="ScoreOrder">
@@ -112,6 +144,7 @@ import { dateFormatter } from '@/utils/formatTime'
 import download from '@/utils/download'
 import * as OrderApi from '@/api/score/order'
 import OrderForm from './OrderForm.vue'
+import OrderDetail from './OrderDetail.vue'
 const message = useMessage() // 消息弹窗
 const { t } = useI18n() // 国际化
 
@@ -121,25 +154,21 @@ const list = ref([]) // 列表的数据
 const queryParams = reactive({
   pageNo: 1,
   pageSize: 10,
-  userId: null,
-  productId: null,
-  number: null,
-  score: null,
-  totalScore: null,
-  ip: null,
-  expressNumber: null,
-  expressCompany: null,
+  orderId: "",
+  type: -1,
   customerName: null,
   customerPhone: null,
   customerAddress: null,
-  status: null,
-  havePaid: null,
-  haveDelivered: null,
-  haveReceived: null,
-  createTime: []
 })
 const queryFormRef = ref() // 搜索的表单
 const exportLoading = ref(false) // 导出的加载中
+const activeName = ref('first')
+const orderStatus = ref('-1')
+
+const queryOrderStatus = (value) => {
+  queryParams.type = value
+  getList()
+}
 
 /** 查询列表 */
 const getList = async () => {
@@ -167,8 +196,17 @@ const resetQuery = () => {
 
 /** 添加/修改操作 */
 const formRef = ref()
+const formRef4 = ref()
 const openForm = (type: string, id?: number) => {
-  formRef.value.open(type, id)
+  if (type == 'updateOrder') {
+    formRef.value.open(type, id)
+  } else if (type == 'orderSend') {
+    formRef1.value.open(type, id)
+  }else if (type == 'sendInfo') {
+    formRef2.value.open(type, id)
+  }else if (type == 'orderDetail') {
+    formRef4.value.open(type, id)
+  }
 }
 
 /** 删除按钮操作 */
@@ -204,3 +242,37 @@ onMounted(() => {
   getList()
 })
 </script>
+<style>
+  .tabBox{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center
+    }
+.tabBox_img{
+    width: 36px;
+    height: 36px;
+  }
+.tabBox_img img{
+  width: 100%;
+  height: 100%;
+}
+.tabBox_tit{
+    /* width :40%; */
+    font-size: 12px !important;
+    margin: 0 2px 0 10px;
+    letter-spacing: 1px;
+    padding: 5px 0;
+    box-sizing: border-box;
+    text-align: left;
+  }
+  .tabBox_pice{
+    /* width :30%; */
+    font-size: 12px !important;
+    margin: 0 2px 0 10px;
+    letter-spacing: 1px;
+    padding: 5px 0;
+    box-sizing: border-box;
+    text-align: left;
+  }
+</style>
